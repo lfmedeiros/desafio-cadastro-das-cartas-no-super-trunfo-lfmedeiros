66d434725496747d25a733bac903d5e9a4f625ea
Lucas Medeiros <50759691+lfmedeiros@users.noreply.github.com>
2025-03-03 18:24:24 +0000

    teste final

--- SuperTrunfo_Novato.c
+++ SuperTrunfo_Novato.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 
 int main() {
-    printf("Super Trunfo - Versão Aventureiro \n");
+    printf("Super Trunfo - Versão Novato \n");
     // Variáveis para a primeira carta
     char codigoCarta1[4];
     int populacao1, pontosTuristicos1;
-    float area1, pib1, densidadePopulacional1, pibPerCapita1;
+    float area1, pib1;
 
     // Variáveis para a segunda carta
     char codigoCarta2[4];
     int populacao2, pontosTuristicos2;
-    float area2, pib2, densidadePopulacional2, pibPerCapita2;
+    float area2, pib2;
 
     // Cadastro da primeira carta
     printf("Cadastro da primeira carta:\n");
@@ -25,10 +25,6 @@ int main() {
     printf("Número de pontos turísticos: ");
     scanf("%d", &pontosTuristicos1);
 
-    // Cálculo dos novos atributos para a primeira carta
-    densidadePopulacional1 = populacao1 / area1;
-    pibPerCapita1 = pib1 * 1000000 / populacao1; // Convertendo PIB para unidades monetárias e calculando per capita
-
     // Cadastro da segunda carta
     printf("\nCadastro da segunda carta:\n");
     printf("Código da carta (ex: B02): ");
@@ -42,18 +38,12 @@ int main() {
     printf("Número de pontos turísticos: ");
     scanf("%d", &pontosTuristicos2);
 
-    // Cálculo dos novos atributos para a segunda carta
-    densidadePopulacional2 = populacao2 / area2;
-    pibPerCapita2 = pib2 * 1000000 / populacao2; // Convertendo PIB para unidades monetárias e calculando per capita
-
     // Exibição dos dados da primeira carta
     printf("\nDados da primeira carta (%s):\n", codigoCarta1);
     printf("População: %d\n", populacao1);
     printf("Área: %.2f km²\n", area1);
     printf("PIB: %.2f milhões\n", pib1);
     printf("Pontos Turísticos: %d\n", pontosTuristicos1);
-    printf("Densidade Populacional: %.2f hab/km²\n", densidadePopulacional1);
-    printf("PIB per Capita: %.2f\n", pibPerCapita1);
 
     // Exibição dos dados da segunda carta
     printf("\nDados da segunda carta (%s):\n", codigoCarta2);
@@ -61,8 +51,6 @@ int main() {
     printf("Área: %.2f km²\n", area2);
     printf("PIB: %.2f milhões\n", pib2);
     printf("Pontos Turísticos: %d\n", pontosTuristicos2);
-    printf("Densidade Populacional: %.2f hab/km²\n", densidadePopulacional2);
-    printf("PIB per Capita: %.2f\n", pibPerCapita2);
 
     return 0;
 }
